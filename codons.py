1de10ef41cb929d95cb3c1480170035470d958ea
Zoeph123 <zoe.philipson2@gmail.com>
2025-12-01 20:16:47 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -20,3 +20,16 @@ def create_codon_dict(file_path):
   return codon_dict
 
 create_codon_dict(path)
+
+
+def test_create_codon_dict():
+    result = create_codon_dict("data/codons.txt")
+
+    assert result['AAA'] == 'K', "Test Failed: 'AAA' should map to 'K'"
+    assert result['AAC'] == 'N', "Test Failed: 'AAC' should map to 'N'"
+    assert result['ACA'] == 'T', "Test Failed: 'ACA' should map to 'T'"
+    assert result['ACC'] == 'T', "Test Failed: 'ACC' should map to 'T'"
+
+    print("All tests passed successfully.")
+
+test_create_codon_dict()
